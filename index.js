15050c191f42ee545cf2e4449f37d7605f1b2a83
Brian Gates <brian.g.gates@gmail.com>
2017-08-02 13:39:14 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { execFile } = require('child_process');
+const path = require('path');
+
+const BIN_PATH = path.resolve(__dirname + '/bin/foo');
+
+const getOutput = (done) => {
+  execFile(BIN_PATH, [], (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    done(stdout);
+  });
+};
+
+exports = getOutput;
